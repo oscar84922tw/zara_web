190c2627827bfd2b8ccb35f7a18e947720c84f34
oscar <oscar84922tw@gmail.com>
2016-12-21 20:31:40 +0800

    tmp

--- zara/female/views.py
+++ zara/female/views.py
@@ -1,28 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from female.models import BranchRequest
 # Create your views here.
+
+
 def main(request):
     return render(request, 'blank.html', {
-    'title': 'blank'
+        'title': 'blank'
     })
+
 
 def design_under_production(request):
     return render(request, 'design-under-production.html')
 
+
 def _fashion(request):
     return render(request, 'fashion.html')
+
 
 def _fb(request):
     return render(request, 'fb.html')
 
+
 def _google(request):
     return render(request, 'google.html')
+
 
 def _list(request):
     return render(request, 'list.html')
 
+
 def _product(request):
-    return render(request, 'product.html')
+    request_products =  BranchRequest.objects.all()
+    return render(request, 'product.html', {'products': request_products})
+
 
 def _region(request):
     return render(request, 'region.html')
